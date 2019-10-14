0819d4946df45ff0fb25457d8e7f81fc193e5247
Syldup <syd9up@gmail.com>
2019-10-14 13:42:43 +0200

    Initial commit

---
+++ precache-manifest.63e8fe14d7858a8199cc2976441598a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "be0b56f20d4d2e68374a",
+    "url": "/js/chunk-vendors.f0419988.js"
+  },
+  {
+    "revision": "28c6adf38e2ab080fd67",
+    "url": "/js/app.67a77721.js"
+  },
+  {
+    "revision": "cf63c70a87786ee97ea9",
+    "url": "/js/about.61c3b3c9.js"
+  },
+  {
+    "revision": "4c90447fc140fd2ce2625b23c4fa3d90",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "28c6adf38e2ab080fd67",
+    "url": "/css/app.5c6aa819.css"
+  }
+];
